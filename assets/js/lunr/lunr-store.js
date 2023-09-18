93c53698f5afb0c9a1b339d210533c59c9cc19ac
Jacob Jaszczak <jacob.jaszczak@gmail.com>
2023-09-17 23:55:26 -0700

    Built site locally

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Markup: Title with Special&nbsp;---&nbsp;Characters",
+        "excerpt":"Putting special characters in the title should have no adverse effect on the layout or functionality. Special characters in the post title have been known to cause issues with JavaScript and XML when not properly encoded and escaped. Latin Character Tests This is a test to see if the fonts...","categories": ["Markup"],
+        "tags": ["html","markup","post","title"],
+        "url": "/markup/markup-title-with-special-characters/",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Markup: Text Alignment",
+        "excerpt":"Default This is a paragraph. It should not have any alignment of any kind. It should just flow like you would normally expect. Nothing fancy. Just straight up text, free flowing, with love. Completely neutral and not picking a side or sitting on the fence. It just is. It just...","categories": ["Markup"],
+        "tags": ["alignment","content","css","markup"],
+        "url": "/markup/markup-text-alignment/",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Markup: Image Alignment",
+        "excerpt":"Welcome to image alignment! The best way to demonstrate the ebb and flow of the various image positioning options is to nestle them snuggly among an ocean of words. Grab a paddle and let’s get started. The image above happens to be centered. The rest of this paragraph is filler for...","categories": ["Markup"],
+        "tags": ["alignment","captions","content","css","image","markup"],
+        "url": "/markup/markup-image-alignment/",
+        "teaser": "/assets/images/500x300.png"
+      }]
